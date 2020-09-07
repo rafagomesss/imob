53b7175fa6cf4305beed5bc15392e5220d9ccf23
Gomes <gomes.webads@gmail.com>
2020-09-07 14:33:18 -0300

    Atualização readme. Início estruturação tela login

--- README.md
+++ README.md
@@ -2,10 +2,13 @@
 <h2> Descrição do Projeto </h2>
 <p>CRUD</p>
 <p>Desenvolvido com o serviço Xampp</p>
+<p>Necessário Composer</p>
+<p>PHP Xampp 7.4</p>
 
 <h2> Etapas </h2>
+<h3>1. Clonar Arquivo em C:/ para que fique C:/imob</h3>
 
-<h3>Criar virtual host</h3>
+<h3>2. Criar virtual host</h3>
 
 **<p>Acessar C:/xampp/apache/conf/extra/httpd-vhosts.conf</p>**
 
@@ -28,3 +31,5 @@
 
 <p>Adicionar ao conteúdo do arquivo:</p>
 127.0.0.1 www.imob.com.br
+
+**_Caso não esteja achando as classes, entre na pasta app e pelo cmd/bash execute o comando composer-dump autoload - o_**
--- app/views/authentication/login.view.php
+++ app/views/authentication/login.view.php
@@ -1,5 +1,15 @@
-<div class="row">
-    <div class="col">
-        Login
+<div class="row justify-content-center">
+    <div class="col-md-10 col-xl-8">
+        <form action="" method="POST">
+            <div class="form-group d-flex">
+                <label for="login">Login</label>
+                <input id="login" name="login" type="text" class="form-control rounded" aria-describedby="helpId" placeholder="">
+            </div>
+            <div class="form-group d-flex">
+                <label for="pass">Senha</label>
+                <input id="pass" name="pass" type="password" class="form-control rounded" aria-describedby="helpId" placeholder="">
+            </div>
+            <button id="btnAccessSystem" class="btn btn-primary">Entrar</button>
+        </form>
     </div>
 </div>
