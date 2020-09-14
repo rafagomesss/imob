e8c5869d6f41060ad50a757eacbf44797fb00787
Gomes <gomes.webads@gmail.com>
2020-09-14 00:34:18 -0300

    Ajuste layout tela registro/edição produtos

--- app/views/products/register.view.php
+++ app/views/products/register.view.php
@@ -5,7 +5,7 @@ use System\Common; ?>
     <div class="col-8">
         <div class="row">
             <div class="col-md-12">
-                <div class="row justify-content-center mt-3 mb-4">
+                <div class="row justify-content-center mt-3 mb-2">
                     <div class="col-md-auto">
                         <h2><?= $this->action === 'edit' ? 'Editar' : 'Cadastrar'; ?> Produto</h2>
                     </div>
